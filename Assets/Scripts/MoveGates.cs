63b7d874c996d48a622eba62bd69540f655ff02e
w1nyx <vad.com.z@gmail.com>
2021-02-10 18:15:32 +0200

    Added chat

---
+++ Assets/Scripts/MoveGates.cs
@@ -0,0 +1,23 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveGates : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != -50f)
+        {
+            transform.position += transform.forward * Time.deltaTime;
+
+        }
+    }
+}
