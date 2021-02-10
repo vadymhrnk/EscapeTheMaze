6a0ab92ad33b8d98b7e117682d81851f2c60d05c
w1nyx <vad.com.z@gmail.com>
2021-02-10 13:47:16 +0200

    First commit

---
+++ Assets/Scripts/CameraMount.cs
@@ -0,0 +1,26 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMount : NetworkBehaviour
+{
+    public GameObject CameraMountPoint;
+
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            Transform cameraTransform = Camera.main.gameObject.transform;
+            cameraTransform.SetParent(CameraMountPoint.transform);
+            cameraTransform.position = CameraMountPoint.transform.position;
+            cameraTransform.rotation = CameraMountPoint.transform.rotation;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
