9e359fe4b556496ee410537c53b9f8fd4ba3ab9e
w1nyx <vad.com.z@gmail.com>
2021-02-16 19:16:13 +0200

    Added lobby scene

---
+++ Assets/Scripts/JoinLobbyMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.UI;
+using TMPro;
+
+public class JoinLobbyMenu : MonoBehaviour
+{
+    [SerializeField] private NetworkManagerLobby networkManager = null;
+
+    [Header("UI")]
+    [SerializeField] private GameObject landingPagePanel = null;
+    [SerializeField] private TMP_InputField ipAddressInputField = null;
+    [SerializeField] private Button joinButton = null;
+
+    private void OnEnable()
+    {
+        NetworkManagerLobby.OnClientConnected += HandleClientConnected;
+        NetworkManagerLobby.OnClientDisconnected += HandleClientDisconnected;
+    }
+
+    private void OnDisable()
+    {
+        NetworkManagerLobby.OnClientConnected -= HandleClientConnected;
+        NetworkManagerLobby.OnClientDisconnected -= HandleClientDisconnected;
+    }
+
+    public void JoinLobby()
+    {
+        string ipAddress = ipAddressInputField.text;
+
+        networkManager.networkAddress = ipAddress;
+        networkManager.StartClient();
+
+        joinButton.interactable = false;
+    }
+
+    private void HandleClientConnected()
+    {
+        joinButton.interactable = true;
+
+        gameObject.SetActive(false);
+        landingPagePanel.SetActive(false);
+    }
+
+    private void HandleClientDisconnected()
+    {
+        joinButton.interactable = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
