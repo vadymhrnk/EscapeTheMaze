b5bc5313948976d64e87ec8e8df4717c0fa63b1b
w1nyx <vad.com.z@gmail.com>
2021-03-12 19:35:26 +0200

    Added new menu feature and game goal(without sync)

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,39 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : NetworkBehaviour
+{
+    [SerializeField] private GameObject WinnerUI = null;
+
+    private string[] password;
+    private int index;
+
+
+    void Start()
+    {
+        password = new string[] { "i", "s", "o", "l", "a","t","e","d" };
+        index = 0;
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (Input.GetKeyDown(password[index]))
+            {
+                index++;
+            }
+            else
+            {
+                index = 0;
+            }
+        }
+
+        if (index == password.Length)
+        {
+            WinnerUI.SetActive(true);
+        }
+    }
+}
